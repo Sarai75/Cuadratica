af60053553e7dae1773bb18630e9546a54def21f
Sarai75 <moaj080602mmcrlsa3@cbtis217.edu.mx>
2024-10-15 19:37:09 -0600

    Codigo elaborado y ejecutado

--- cuadartica.cpp
+++ cuadartica.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double A, B, C;
+    double d;
+    double x1, x2;
 
+    cout<<"Coloca los valores de A, B y C: "<<endl;
+    cin>>A;
+    cin>>B;
+    cin>>C;
+
+    d=B*B-4*A*C;
+
+
+    if(d>0)
+    {
+        x1 = (-B+sqrt(d))/(2*A);
+        x2 = (-B-sqrt(d))/(2*A);
+        cout<<"Las raíces son: x1 = "<<x1<<" y x2 = "<<x2<<endl;
+    }
+    else if(d==0)
+    {
+        x1=-B/(2*A);
+        cout<<"La raíz doble es: x1 = x2 = " <<x1<< endl;
+    }
+    else
+    {
+        cout<<"Las raíces son complejas."<<endl;
+    }
+    return 0;
+}
